fc4cc8cc56eac1ee83a3a38f3e7c41e86ca80342
riccardo <r.iervolino93@gmail.com>
2019-01-24 16:22:39 +0100

    documentation

---
+++ Resources/Levels/Radme/Cal/Blueprints/TileSet/Miky Blue Print.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Miky Blue Print" tilewidth="32" tileheight="32" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="azure.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="gray.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="green.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="white.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="black.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="rust.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="violet.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="enemy.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="vision.png"/>
+ </tile>
+</tileset>
